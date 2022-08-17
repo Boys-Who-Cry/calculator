78d633942378fc013ef224c9578a7ce9a2694253
Michael <edu.michaelstetter@gmail.com>
2022-08-17 11:20:18 -0400

    Created functionality to store two operands in their own variables, and updates calculator display accordingly. Pick and stores operator in its own variable.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
 const calculatorOutput = document.querySelector("#calculatorOutput");
+let output = calculatorOutput.textContent;
 const clear = document.querySelector("[data-clear]");
 const equals = document.querySelectorAll("[data-equals]");
 const actionButton = document.querySelectorAll("[data-action]");
 const numberButton = document.querySelectorAll("[data-number]");
 
+let previousOperand = "";
+let currentOperand = "";
+let operator = "";
+let results;
+
+const updateDisplay = (str) => {
+    output = str;
+    calculatorOutput.textContent = output;
+}
 
 const multiply = (num1, num2) => {
 }
@@ -15,20 +25,51 @@ const subtract = (num1, num2) => {
 }
 
 const operate = (firstNumber, secondNumber, operator) => {
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
     switch(operator){
         case "*":
-            multiply(firstNumber, secondNumber);
+            results = multiply(firstNumber, secondNumber);
             break;
         case "/":
-            divide(firstNumber, secondNumber);
+            results = divide(firstNumber, secondNumber);
             break;
         case "+":
-            add(firstNumber, secondNumber);
+            results = add(firstNumber, secondNumber);
             break;
         case "-":
-            minus(firstNumber, secondNumber);
+            results = minus(firstNumber, secondNumber);
             break;
     }
+    updateDisplay(results);
 }
 
-console.log(clear);
+
+
+//Numbers
+numberButton.forEach(button => {
+    button.addEventListener("click", (event) => {
+        //Gets second number
+        if(operator && previousOperand === ""){
+            previousOperand = currentOperand;
+            currentOperand = "";
+        }
+        //First Number
+        currentOperand += event.target.textContent;
+
+        updateDisplay(currentOperand);
+
+        console.log(`Previous: ${previousOperand} Current: ${currentOperand} Operator: ${operator}`);
+    });
+});
+
+
+
+//Operator
+actionButton.forEach(button => {
+    button.addEventListener("click", (event) => {
+        if(!currentOperand){return;}//Forbids choosing operator unless it proceeds a number.
+        operator = event.target.dataset.action;
+        //console.log(`Operator: ${operator}`);
+    });
+})
