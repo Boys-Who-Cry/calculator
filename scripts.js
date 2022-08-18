02641658a7f8a2603b2014ee995122396a886226
Michael <edu.michaelstetter@gmail.com>
2022-08-17 23:24:50 -0400

    Calculator completed. Some minor things to fix.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const calculatorOutput = document.querySelector("#calculatorOutput");
 let output = calculatorOutput.textContent;
 const clear = document.querySelector("[data-clear]");
-const equals = document.querySelectorAll("[data-equals]");
+const equals = document.querySelector("[data-equals]");
 const actionButton = document.querySelectorAll("[data-action]");
 const numberButton = document.querySelectorAll("[data-number]");
 
@@ -15,33 +15,33 @@ const updateDisplay = (str) => {
     calculatorOutput.textContent = output;
 }
 
-const multiply = (num1, num2) => {
-}
-const divide = (num1, num2) => {
-}
-const add = (num1, num2) => {
-}
-const subtract = (num1, num2) => {
-}
+const multiply = (num1, num2) => {return num1 * num2;}
+const divide = (num1, num2) => {return num1 / num2;}
+const add = (num1, num2) => {return num1 + num2;}
+const subtract = (num1, num2) => {return num1 - num2;}
 
-const operate = (firstNumber, secondNumber, operator) => {
-    firstNumber = Number(firstNumber);
-    secondNumber = Number(secondNumber);
-    switch(operator){
+const operate = (prev, current, op) => {
+    prev = Number(prev);
+    current = Number(current);
+    switch(op){
         case "*":
-            results = multiply(firstNumber, secondNumber);
+            results = multiply(prev, current);
+            updateDisplay(results);
             break;
         case "/":
-            results = divide(firstNumber, secondNumber);
+            results = divide(prev, current);
+            updateDisplay(results);
             break;
         case "+":
-            results = add(firstNumber, secondNumber);
+            results = add(prev, current);
+            updateDisplay(results);
             break;
         case "-":
-            results = minus(firstNumber, secondNumber);
+            results = subtract(prev, current);
+            updateDisplay(results);
             break;
     }
-    updateDisplay(results);
+    return results;
 }
 
 
@@ -68,8 +68,42 @@ numberButton.forEach(button => {
 //Operator
 actionButton.forEach(button => {
     button.addEventListener("click", (event) => {
-        if(!currentOperand){return;}//Forbids choosing operator unless it proceeds a number.
+        if(operator){
+            getResults();
+        }
+        else if(previousOperand){
+            operator = event.target.dataset.action;
+        }
+        else if(!currentOperand){return;}//Forbids choosing operator unless it proceeds a number.
         operator = event.target.dataset.action;
-        //console.log(`Operator: ${operator}`);
+        console.log(`Operator: ${operator}`);
     });
 })
+
+
+
+//Clear
+clear.addEventListener("click", () => {
+    previousOperand = "";
+    currentOperand = "";
+    operator = "";
+    updateDisplay("0");
+    //console.log(`Previous: ${previousOperand} Current: ${currentOperand} Operator: ${operator}`);
+});
+
+
+
+//Equals
+//The reasoning behind using another function to perform the functions of the equals button and calling the below getResults() function from inside of the equals button event listener is to be able to call it when chaining equations not using the equals button.
+const getResults = () => {
+    let results = operate(previousOperand, currentOperand, operator);
+
+    previousOperand = results;
+    currentOperand = "";
+    operator = "";
+    console.log(`Previous: ${previousOperand} Current: ${currentOperand} Operator: ${operator}`);
+}
+equals.addEventListener("click", () => {
+    //Storing the return value of the operate function in its own variable and then reorganizing/resetting the calculators variables as such, enables the user to use the equals button to chain different equations.
+    getResults();
+});
